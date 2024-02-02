4d869772960ddae77d316868207b4171bebdce20
MarkusTHolm <mtaho@dtu.dk>
2024-02-02 14:57:38 +0100

    Completed Himmelblau exercise

--- mtaho/src/BasicOpt.py
+++ mtaho/src/BasicOpt.py
@@ -11,29 +11,45 @@ class Opt:
 
     def contourPlot(fun, outPath=None,
                     xlim=[-10, 10], ylim=[-10, 10], nPoints=400,
-                    fontSize = 20, figSize = (7, 5)):
+                    fontSize = 20, figSize = (7, 5), 
+                    colorScale='linear', vmax=None):
 
         x = np.linspace(xlim[0], xlim[1], nPoints)
         y = np.linspace(ylim[0], ylim[1], nPoints)
         X, Y = np.meshgrid(x, y)
         Z = fun(X, Y)
 
+        if colorScale == 'linear':
+            norm = 'linear'
+            nlevels = 20
+            levels = np.arange(0, vmax, vmax/nlevels)
+            locator=ticker.LinearLocator()
+        elif colorScale == 'log':
+            norm = LogNorm()
+            locator = ticker.LogLocator()
+            levels = 1
+
         # Plot results
         define_plot_settings(fontSize)
 
         # Contour plot
-        fig, ax = plt.subplots(figsize = figSize )
-        cs = ax.contourf(X, Y, Z, 200,\
-                         norm=LogNorm())#locator=ticker.LogLocator())
+        fig, ax = plt.subplots(figsize = figSize)
+        cs = ax.contourf(X, Y, Z,
+                         levels=levels,
+                         norm=norm)
         ax.contour(X, Y, Z,\
-                   locator=ticker.LogLocator(),
+                   levels = levels,
+                   locator=locator,
                    linewidths=.25,
                    colors='w')
+        
         cbar = fig.colorbar(cs)
+
         ax.set_xlabel('$x_1$')
         ax.set_ylabel('$x_2$')
-        # ax.grid()
         fig.tight_layout()
         
         if not outPath == None:
             fig.savefig(outPath)
+
+        return fig, ax
---
+++ mtaho/w01/himmelbau.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+from scipy.optimize import minimize, Bounds
+from scipy.optimize import LinearConstraint, NonlinearConstraint
+
+projectDir = "/home/mtaho/OneDrive/PhD/08_Courses/ConstrainedOptimization/Code"
+sys.path.append(os.path.realpath(f"{projectDir}"))
+
+from mtaho.plot_settings import define_plot_settings
+from mtaho.src.FiniteDifference import FD
+from mtaho.src.BasicOpt import Opt
+
+workDir = f"{projectDir}/mtaho/w01"
+
+def contourFunHimmelblau(X, Y):
+    tmp1 = X**2 + X - 11
+    tmp2 = X + Y**2 - 7
+    f = tmp1**2 + tmp2**2
+    return f
+
+x0 = np.array([-4, 0])
+xLower = [-5, -5]
+xUpper = [5, 5]
+bounds = Bounds(xLower, xUpper)
+
+# Version 1 - No gradients and no constraints
+def objFunHimmelblau(x):
+    tmp1 = x[0]**2 + x[1] - 11
+    tmp2 = x[0] + x[1]**2 - 7
+    f = tmp1**2 + tmp2**2
+    return f
+
+print("\n Results of version 1: ")
+res1 = minimize(objFunHimmelblau, x0,
+               method='trust-constr', 
+               options={'verbose': 1}, bounds=bounds)
+
+# Version 2 - With gradients
+def objFunGradHimmelblau(x):
+    tmp1 = x[0]**2 + x[1] - 11
+    tmp2 = x[0] + x[1]**2 - 7
+    f = tmp1**2 + tmp2**2
+    df = np.array([4*(x[0]**2 + x[1] - 11)*x[0] + 2*(x[0] + x[1]**2 - 7),
+                   2*(x[0]**2 + x[1] - 11) + 4*(x[0] + x[1]**2 - 7)*x[1]])
+    return (f, df)
+
+print("\n Results of version 2: ")
+res2 = minimize(objFunGradHimmelblau, x0,jac=True,
+               method='trust-constr', 
+               options={'verbose': 1}, bounds=bounds)
+
+# Version 3 - With gradients and constraints
+
+# c1 = (x1 + 2)^2 - x2 >= 0 (Nonlinear constraint)
+def cons_f(x):
+    return (x[0] + 2)**2 - x[1]
+def cons_J(x):
+    return [2*(x[0] + 2), - 1]
+nonlinear_constraint = NonlinearConstraint(cons_f, 0, np.inf, jac=cons_J)
+
+# c2 = -4*x1 + 10*x2 (Linear constraint)
+A = [-4, 10]
+linear_constraint = LinearConstraint(A, [0], [np.inf])
+
+print("\n Results of version 3: ")
+res3 = minimize(objFunGradHimmelblau, x0, jac=True,
+                method='trust-constr',
+                constraints=[linear_constraint, nonlinear_constraint], 
+                options={'verbose': 1}, bounds=bounds)
+
+
+# Plot solutions
+fig, ax = Opt.contourPlot(contourFunHimmelblau,
+                          xlim=[-5, 5], ylim=[-5, 5], vmax=200,
+                          colorScale='linear', figSize=(8, 5))
+
+ax.plot(x0[0], x0[1], 'go', label='$x^0$')
+ax.plot(res1.x[0], res1.x[1], 'r*', label=r'$x^\ast_1$', markersize=10)
+ax.plot(res2.x[0], res2.x[1], 'ms', markerfacecolor='none', markersize=10, 
+        label=r'$x^\ast_2$', markeredgewidth=2)
+ax.plot(res3.x[0], res3.x[1], 'b^', markerfacecolor='none', markersize=10, 
+        label=r'$x^\ast_3$', markeredgewidth=2)
+
+# Draw constraints
+xc = np.linspace(-5, 5)
+alpha = 0.5
+yc1 = (xc+2)**2
+yc2 = (4*xc)/10
+ax.fill(xc, yc1, "c", color='grey', alpha=alpha, label='$c_1$',
+        hatch='\\') 
+ax.fill_between(xc, yc2, np.repeat(-5, 50), color='grey', alpha=alpha, label='$c_2$',
+                hatch='/') 
+
+ax.set_xlim([-5, 5])
+ax.set_ylim([-5, 5])
+
+ax.legend(bbox_to_anchor=(1.5, 1))
+fig.tight_layout()
+fig.savefig(f'{workDir}/himmelblauContour.png')
